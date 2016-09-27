f9b3ed427a9d6d02c803a169adfab3be4f599305
Fabrice JANOTY <fjanoty@e2r1p13.42.fr>
2016-09-27 12:50:03 +0200

    debut de norme

--- libft/ft_strcpy.c
+++ libft/ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: jpirsch <jpirsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 16:19:29 by jpirsch           #+#    #+#             */
-/*   Updated: 2016/09/10 05:32:44 by fjanoty          ###   ########.fr       */
+/*   Updated: 2016/09/27 12:16:08 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,12 @@ char	*ft_strcpy(char *dst, const char *src)
 	int i;
 
 	i = 0;
-//	dprintf(1, "##begin cpy\n");
 	while (src[i])
 	{
-//		dprintf(1, " i:%d ", i);
 		dst[i] = src[i];
 		i++;
 		if (!src[i])
 			dst[i] = src[i];
 	}
-//	dprintf(1, "##end cpy\n");
 	return (dst);
 }
